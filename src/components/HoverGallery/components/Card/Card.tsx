5b6c8639defd172d5d6ca85465643629fdfa9870
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-17 20:29:09 +0200

    Added hover effect

--- src/components/HoverGallery/components/Card/Card.tsx
+++ src/components/HoverGallery/components/Card/Card.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import styles from "./Card.module.scss";
+import CardProps from "./CardProps";
+import { motion } from "framer-motion";
 
-const Card = () => {
-  return <div className={styles.cardWrapper}>Card</div>;
+const Card = (props: CardProps) => {
+  const { isGalleryActive, numberOfSlides } = props;
+
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <motion.div
+      animate={{
+        width:
+          isGalleryActive && isActive ? "100%" : `${100 / numberOfSlides}%`,
+      }}
+      transition={{ duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }}
+      className={styles.cardWrapper}
+      onMouseEnter={() => {
+        setIsActive(true);
+      }}
+      onMouseLeave={() => {
+        setIsActive(false);
+      }}
+    >
+      Card
+    </motion.div>
+  );
 };
 
 export default Card;
---
+++ src/components/HoverGallery/components/Card/CardProps.ts
@@ -0,0 +1,6 @@
+import { Dispatch, SetStateAction } from "react";
+
+export default interface CardProps {
+  numberOfSlides: number;
+  isGalleryActive: boolean;
+}
